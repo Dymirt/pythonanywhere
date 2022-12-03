818b7b8fa7ca6bb2fbb59b8f1ab504bafaf8adbd
Dymirt <kolidadmytro@gmail.com>
2022-12-03 13:01:41 +0100

    minor fix

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'auctions'
 
 urlpatterns = [
-    path("", views.ListingListView.as_view(), name="index2"),
+    path("", views.ListingListView.as_view(), name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
