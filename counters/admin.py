da56f166dfd2d8d43e5b137a601a0de90498666b
Dymirt <kolidadmytro@gmail.com>
2023-07-16 19:30:33 +0200

    adding user to counters
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- counters/admin.py
+++ counters/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Counter, Reading
 
 # Register your models here.
-admin.site.register(Counter)
 admin.site.register(Reading)
+
+
+@admin.register(Counter)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("title", "user")
--- counters/models.py
+++ counters/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 
 
 class Counter(models.Model):
-    #user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="counters", blank=True, null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="counters", blank=True, null=True)
     title = models.CharField(max_length=60)
     consumable = models.BooleanField(default=True)
     unit = models.CharField(max_length=60, blank=True, null=True)
@@ -15,7 +15,7 @@ class Counter(models.Model):
     fixed_price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}-{self.user}"
 
 
 class Reading(models.Model):
